9c95bb93bb67d94a5bb83677b5c9bf3ef084b8d5
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2022-10-02 21:21:37 +0000

    [fix] modules

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-import os
-import setuptools
+from setuptools import find_packages, setup, find_namespace_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -7,21 +6,22 @@ with open("README.md", "r") as fh:
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-setuptools.setup(
-    name = "toolkit4life",                                  # This is the name of the package
-    version = "0.0.2",                                      # The initial release version
-    author = "Keivan Ipchi Hagh",                           # Full name of the author
+setup(
+    name = "toolkit4life",                                      # This is the name of the package
+    version = "0.0.72",                                         # The initial release version
+    author = "Keivan Ipchi Hagh",                               # Full name of the author
+    url = "https://github.com/keivanipchihagh/toolkit4life",    # URL to the github repository
     description = "Faster deployment is what we want!",
-    long_description = long_description,                    # Long description read from the the readme file
+    long_description = long_description,                        # Long description read from the the readme file
     long_description_content_type = "text/markdown",
-    packages = setuptools.find_packages(),                  # List of all python modules to be installed
+    packages = find_namespace_packages(include = ["toolkit4life.*"]),   # List of all python modules to be installed
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],                                                      # Information to filter the project on PyPi website
-    python_requires = '>=3.6',                              # Minimum version requirement of the package
-    py_modules = ["toolkit4life"],                          # Name of the python package
-    package_dir = {'':'toolkit4life'},                      # Directory of the source code of the package
-    install_requires = required                             # Install other dependencies if any
+    ],                                                          # Information to filter the project on PyPi website
+    python_requires = '>=3.6',                                  # Minimum version requirement of the package
+    py_modules = ["toolkit4life"],                              # Name of the python package
+    # package_dir = {'':'toolkit4life'},                        # Directory of the source code of the package
+    install_requires = required                                 # Install other dependencies if any
 )
