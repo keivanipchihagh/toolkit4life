83b919c16031c01535c3d887dfe39e46e029a5d5
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2022-10-13 15:36:22 +0000

    update

--- toolkit4life/clients/redis.py
+++ toolkit4life/clients/redis.py
@@ -35,37 +35,6 @@ class RedisClient():
         # Clear the database if fresh_start is set to true
         if fresh_start:
             self.engine.flushdb()
-    
-
-    def keys(self, pattern: str) -> list:
-        """ Returns the keys that matches the pattern """
-        return self.engine.keys(pattern)
-
-
-    def close(self) -> None:
-        """ Closes the redis connection """
-        self.engine.close()
-
-
-    def get_all_keys(self) -> list:
-        """ All the available keys """
-        return self.engine.keys("*")
-
-
-    def get_values_from_keys(self, keys: list) -> list:
-        """ Returns the values for the given keys """
-        with self.engine.pipeline() as pipe:
-            for key in keys:
-                pipe.hgetall(key)
-            return [records for records in pipe.execute()]
-
-
-    def delete_values_from_keys(self, keys: list) -> None:
-        """ Deletes the given keys """
-        with self.engine.pipeline() as pipe:
-            for key in keys:
-                pipe.delete(key)
-            pipe.execute()
 
 
     def test_connection(self) -> None:
@@ -73,8 +42,69 @@ class RedisClient():
         self.engine.ping()
 
 
-    def insert_dict(self, key: str, value: dict) -> None:
-        """ Inserts the given key and a dictionary """
+    def close(self) -> None:
+        """ Closes the redis connection """
+        self.engine.close()
+
+
+    def get_keys_from_pattern(self, pattern: str) -> list:
+        """ Returns keys that match the given pattern """
+        return self.engine.keys(pattern)
+
+
+    def get_keys_all(self) -> list:
+        """ Returns all the keys """
+        return self.engine.keys("*")
+
+
+    def get_values_from_list(self, keys: list) -> list:
+        """ Returns the values for the given list of keys """
+        with self.engine.pipeline() as pipe:
+            for key in keys:
+                pipe.hgetall(key)
+            return [records for records in pipe.execute()]
+
+
+    def get_values_from_pattern(self, pattern: str) -> list:
+        """ Returns the values for the given pattern """
+        return self.get_values_from_list(self.get_keys_from_pattern(pattern))
+
+
+    def get_values_all(self) -> list:
+        """ Returns all the values """
+        return self.get_values_from_list(self.get_keys_all())
+
+
+    def get_value_from_key(self, key: str) -> dict:
+        """ Returns the value for the given key """
+        return self.engine.hgetall(key)
+
+
+    def get_dict_all(self) -> dict:
+        """ Returns the keys and values as a dictionary """
+        return dict(zip(self.get_all_keys(), self.get_all_values()))
+
+
+    def get_dict_from_list(self, keys = list) -> dict:
+        """ Returns the keys and values as a dictionary """
+        return dict(zip(keys, self.get_values_from_keys(keys)))
+
+
+    def delete_values_from_list(self, keys: list) -> None:
+        """ Deletes the given list of keys """
+        with self.engine.pipeline() as pipe:
+            for key in keys:
+                pipe.delete(key)
+            pipe.execute()
+
+
+    def delete_key(self, key: str) -> None:
+        """ Deletes the given key """
+        self.engine.delete(key)
+
+
+    def insert_key_value(self, key: str, value: dict) -> None:
+        """ Inserts the given key and value """
         self.engine.hmset(key, value)
 
 
